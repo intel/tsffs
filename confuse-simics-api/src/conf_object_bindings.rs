821e38b6d6fa1bf0c3b1b8d1591304cd1168f9a8
novafacing <rowan.hart@intel.com>
2023-04-14 16:36:47 -0700

    Remove confuse simics API

--- confuse-simics-api/src/conf_object_bindings.rs
+++
@@ -1,6 +0,0 @@
-use crate::{conf_class_t, conf_object_t, sobject_class};
-
-#[no_mangle]
-pub unsafe extern "C" fn SIM_object_class(obj: *const conf_object_t) -> *mut conf_class_t {
-    sobject_class(&unsafe { *obj }.sobj) as *mut conf_class_t
-}
