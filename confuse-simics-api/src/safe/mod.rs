5fab22fa33cc45135eb8645812d22c1093dcea60
novafacing <rowan.hart@intel.com>
2023-05-02 16:47:21 -0700

    Delete legacy confuse simics API crate

--- confuse-simics-api/src/safe/mod.rs
+++
@@ -1,7 +0,0 @@
-//! A small safe API around SIMICS APIs that are used in the CONFUSE project.
-//! Feel free to add more safe wrappers as you see fit
-//!
-
-pub mod common;
-pub mod types;
-pub mod wrapper;
--- confuse-simics-api/src/safe/types.rs
+++
@@ -1,3 +0,0 @@
-use crate::hap_handle_t;
-
-pub type HapHandle = hap_handle_t;
