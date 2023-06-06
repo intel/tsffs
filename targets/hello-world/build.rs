319325cba8ea29d78a1d9c6add424428432fbd1c
novafacing <rowan.hart@intel.com>
2023-06-06 12:54:01 -0700

    Update edk2 repo hash and edk2 build container version and hash

--- targets/hello-world/build.rs
+++ targets/hello-world/build.rs
@@ -11,7 +11,6 @@ use std::{
 
 const EDK2_REPO_URL: &str = "https://github.com/tianocore/edk2.git";
 const EDK2_REPO_HASH: &str = "d189de3b0a2f44f4c9b87ed120be16569ea19b51";
-const EDK2_REPO_HASH: &str = "02fcfdce1e5ce86f1951191883e7e30de5aa08be";
 const EDK2_FEDORA37_REPO_URL: &str = "ghcr.io/tianocore/containers/fedora-37-build";
 const EDK2_FEDORA37_BUILD_TAG: &str = "a0dd931";
 
