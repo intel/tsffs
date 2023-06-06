554ba5f0fda85afbb5563e8afb675814f45d13aa
novafacing <rowan.hart@intel.com>
2023-06-06 11:25:52 -0700

    Add safe api modules for to-be-implemented header directories

--- simics-api/src/safe/mod.rs
+++ simics-api/src/safe/mod.rs
@@ -1,19 +1,25 @@
+pub mod arch;
 pub mod base;
+pub mod devs;
 pub mod interface;
 pub mod internal;
 pub mod logging;
 pub mod model_interface;
 pub mod processor;
 pub mod simulator;
+pub mod simulator_interface;
 pub mod traits;
 pub mod util;
 
 pub use self::logging::*;
+pub use arch::*;
 pub use base::*;
+pub use devs::*;
 pub use interface::*;
 pub use internal::*;
 pub use model_interface::*;
 pub use processor::*;
 pub use simulator::*;
+pub use simulator_interface::*;
 pub use traits::*;
 pub use util::*;
