5e539ab2697a5be0cf88b272636c90a0f89c42e4
novafacing <rowan.hart@intel.com>
2023-07-11 13:00:05 -0700

    Format C/CPP

--- examples/x509-parse/src/confuse.h
+++ examples/x509-parse/src/confuse.h
@@ -14,7 +14,7 @@
 /// This macro performs a CPUID instruction with an input (in this case a magic value)
 /// as well as setting the (r/e)si/(r/e)di registers to a specific value (in_0, in_1) in
 /// order to pass information to the simulator
-#define __cpuid_extended2(level, a, b, c, d, inout_ptr_0, inout_ptr_1)                  \
+#define __cpuid_extended2(level, a, b, c, d, inout_ptr_0, inout_ptr_1)                 \
     do {                                                                               \
         if (__builtin_constant_p(level) && (level) != 1)                               \
             __asm__ __volatile__("cpuid\n\t"                                           \
@@ -26,17 +26,17 @@
                                  : "=a"(a), "=b"(b), "=c"(c), "=d"(d)                  \
                                  : "0"(level), "1"(0), "2"(0), "S"(in_0), "D"(in_1));  \
     } while (0)
-#define __cpuid_extended1(level, a, b, c, d, inout_ptr_0)                  \
+#define __cpuid_extended1(level, a, b, c, d, inout_ptr_0)                              \
     do {                                                                               \
         if (__builtin_constant_p(level) && (level) != 1)                               \
             __asm__ __volatile__("cpuid\n\t"                                           \
                                  : "=a"(a), "=b"(b), "=c"(c), "=d"(d),                 \
                                    "=S"(*inout_ptr_0), "=D"(*inout_ptr_1)              \
-                                 : "0"(level), "S"(*inout_ptr_2));  \
+                                 : "0"(level), "S"(*inout_ptr_2));                     \
         else                                                                           \
             __asm__ __volatile__("cpuid\n\t"                                           \
                                  : "=a"(a), "=b"(b), "=c"(c), "=d"(d)                  \
-                                 : "0"(level), "1"(0), "2"(0), "S"(in_0));  \
+                                 : "0"(level), "1"(0), "2"(0), "S"(in_0));             \
     } while (0)
 #define __cpuid(level, a, b, c, d)                                                     \
     do {                                                                               \
@@ -53,14 +53,14 @@
 /// This macro performs a CPUID instruction with an input (in this case a magic value)
 /// as well as setting the (r/e)si/(r/e)di registers to a specific value (in_0, in_1) in
 /// order to pass information to the simulator
-#define __cpuid_extended2(level, a, b, c, d, inout_ptr_0, inout_ptr_1)                  \
+#define __cpuid_extended2(level, a, b, c, d, inout_ptr_0, inout_ptr_1)                 \
     __asm__ __volatile__("cpuid\n\t"                                                   \
                          : "=a"(a), "=b"(b), "=c"(c), "=d"(d), "=S"(*inout_ptr_0),     \
                            "=D"(*inout_ptr_1)                                          \
                          : "0"(level), "S"(*inout_ptr_0), "D"(*inout_ptr_1))
-#define __cpuid_extended1(level, a, b, c, d, inout_ptr_0)                  \
+#define __cpuid_extended1(level, a, b, c, d, inout_ptr_0)                              \
     __asm__ __volatile__("cpuid\n\t"                                                   \
-                         : "=a"(a), "=b"(b), "=c"(c), "=d"(d), "=S"(*inout_ptr_0)     \
+                         : "=a"(a), "=b"(b), "=c"(c), "=d"(d), "=S"(*inout_ptr_0)      \
                          : "0"(level), "S"(*inout_ptr_0))
 #define __cpuid(level, a, b, c, d)                                                     \
     __asm__ __volatile__("cpuid\n\t" : "=a"(a), "=b"(b), "=c"(c), "=d"(d) : "0"(level))
@@ -90,7 +90,7 @@
     do {                                                                               \
         uint32_t _a, _b, _c, _d;                                                       \
         uint32_t cpuid_input = (CONFUSE_START_SIGNAL << 16U) | SIMICS_MAGIC_CPUID;     \
-        __cpuid_extended2(cpuid_input, _a, _b, _c, _d, addr_ptr, size_ptr);             \
+        __cpuid_extended2(cpuid_input, _a, _b, _c, _d, addr_ptr, size_ptr);            \
     } while (0)
 
 #define HARNESS_STOP()                                                                 \
@@ -99,11 +99,11 @@
         uint32_t _a, _b, _c, _d;                                                       \
         __cpuid(cpuid_input, _a, _b, _c, _d);                                          \
     } while (0)
-#define HARNESS_STOP_EXTENDED(val_ptr)                                    \
+#define HARNESS_STOP_EXTENDED(val_ptr)                                                 \
     do {                                                                               \
         uint32_t cpuid_input = (CONFUSE_STOP_SIGNAL << 16U) | SIMICS_MAGIC_CPUID;      \
         uint32_t _a, _b, _c, _d;                                                       \
-        __cpuid_extended1(cpuid_input, _a, _b, _c, _d, val_ptr);          \
+        __cpuid_extended1(cpuid_input, _a, _b, _c, _d, val_ptr);                       \
     } while (0)
 
 #else // defined(__GNUC__) && defined(__x86_64__)
