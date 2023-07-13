744004090ac82eb2c85667548566f852f8e6df66
novafacing <rowan.hart@intel.com>
2023-07-13 10:24:52 -0700

    Fix container builder jobs

--- examples/hello-world/build.sh
+++ examples/hello-world/build.sh
@@ -5,13 +5,13 @@
 # otherwise, the EFI is included in the source tree for ease of use
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-ROOT_DIR="${SCRIPT_DIR}/../../"
 IMAGE_NAME="edk2-build-hello-world"
 CONTAINER_UID=$(echo "${RANDOM}" | sha256sum | head -c 8)
 CONTAINER_NAME="${IMAGE_NAME}-tmp-${CONTAINER_UID}"
 
-pushd "${ROOT_DIR}"
-docker build -t "${IMAGE_NAME}" -f "${SCRIPT_DIR}/Dockerfile" "${ROOT_DIR}"
+pushd "${SCRIPT_DIR}"
+
+docker build -t "${IMAGE_NAME}" -f "Dockerfile" .
 docker create --name "${CONTAINER_NAME}" "${IMAGE_NAME}"
 docker cp \
     "${CONTAINER_NAME}:/edk2/HelloWorld/Build/HelloWorld/DEBUG_GCC5/X64/HelloWorld.efi" \
