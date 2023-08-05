9cce336a2a571a0ba1cf98723cf9caadaab53b51
novafacing <rowan.hart@intel.com>
2023-08-04 23:56:17 -0700

    Add video source and scripts

---
+++ docs/videos/triage/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+pushd "${SCRIPT_DIR}" || exit 1
+
+cargo run --manifest-path ../adv/Cargo.toml --bin adv triage.toml
