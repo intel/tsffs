38e4c8c6e8ec43e7bc7dce0e9c8eff42e5172e63
novafacing <rowanbhart@gmail.com>
2023-11-29 08:26:58 -0800

    Fix globbing

--- examples/tests/build.sh
+++ examples/tests/build.sh
@@ -2,7 +2,8 @@
 
 if [ -z "${SIMICS_BASE}" ]; then
     echo "SIMICS_BASE is not set, defaulting to latest."
-    export SIMICS_BASE=$(ispm packages --list-installed --json | jq -r '[ .installedPackages[] | select(.pkgNumber == 1000) ] | ([ .[].version ] | max_by(split(".") | map(tonumber))) as $m | first(first(.[]|select(.version == $m)).paths[0])')
+    SIMICS_BASE="$(ispm packages --list-installed --json | jq -r '[ .installedPackages[] | select(.pkgNumber == 1000) ] | ([ .[].version ] | max_by(split(".") | map(tonumber))) as $m | first(first(.[]|select(.version == $m)).paths[0])')"
+    export "${SIMICS_BASE}"
 fi
 
 if [ ! -d "${SIMICS_BASE}" ]; then
@@ -14,6 +15,6 @@ for TARGET in *; do
     if [ -d "${TARGET}" ]; then
         pushd "${TARGET}" || exit 1
         ./build.sh
-        popd
+        popd || exit 1
     fi
 done
