968083f05f3240272f39aee1f1394791fadf62db
novafacing <rowanbhart@gmail.com>
2023-11-29 08:37:59 -0800

    Fix shellcheck

--- examples/tests/build.sh
+++ examples/tests/build.sh
@@ -3,7 +3,7 @@
 if [ -z "${SIMICS_BASE}" ]; then
     echo "SIMICS_BASE is not set, defaulting to latest."
     SIMICS_BASE="$(ispm packages --list-installed --json | jq -r '[ .installedPackages[] | select(.pkgNumber == 1000) ] | ([ .[].version ] | max_by(split(".") | map(tonumber))) as $m | first(first(.[]|select(.version == $m)).paths[0])')"
-    export "${SIMICS_BASE}"
+    export SIMICS_BASE
 fi
 
 if [ ! -d "${SIMICS_BASE}" ]; then
